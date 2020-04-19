7aceafeede59b4cba370a2271c1b4c85532bb7f9
xiifwat <xiifwat@gmail.com>
2020-04-19 11:54:12 +0600

    Forgot password UI added

---
+++ app/src/main/java/com/bs/ecommerce/auth/forgotpass/ForgotPasswordFragment.kt
@@ -0,0 +1,16 @@
+package com.bs.ecommerce.auth.forgotpass
+
+import android.widget.RelativeLayout
+import com.bs.ecommerce.R
+import com.bs.ecommerce.base.BaseFragment
+import com.bs.ecommerce.base.BaseViewModel
+import com.bs.ecommerce.main.MainViewModel
+import kotlinx.android.synthetic.main.fragment_forgot_password.*
+
+class ForgotPasswordFragment : BaseFragment() {
+    override fun getLayoutId(): Int = R.layout.fragment_forgot_password
+
+    override fun getRootLayout(): RelativeLayout? = forgotPasswordRootLayout
+
+    override fun createViewModel(): BaseViewModel = MainViewModel()
+}
