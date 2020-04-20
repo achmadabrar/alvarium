42677489ff4acaf71ae26948c7deea9f976201c0
Ruhul Rahat <ruhul.rahat@bs-23.net>
2020-04-20 20:05:31 +0600

    settings fragment baseUrl change added

--- app/src/main/java/com/bs/ecommerce/more/SettingsFragment.kt
+++ app/src/main/java/com/bs/ecommerce/more/SettingsFragment.kt
@@ -3,6 +3,8 @@ package com.bs.ecommerce.more
 import android.os.Bundle
 import android.view.View
 import android.widget.RelativeLayout
+import android.widget.Spinner
+import android.widget.SpinnerAdapter
 import androidx.lifecycle.ViewModelProvider
 import com.bs.ecommerce.R
 import com.bs.ecommerce.base.BaseFragment
@@ -12,8 +14,17 @@ import com.bs.ecommerce.main.model.MainModelImpl
 import com.bs.ecommerce.more.viewmodel.BaseUrlChangeFragment
 import com.bs.ecommerce.utils.hideKeyboard
 import kotlinx.android.synthetic.main.fragment_settings.*
+import java.util.ArrayList
 
 class SettingsFragment: BaseUrlChangeFragment() {
+
+
+    //lateinit var response: GetLanguageResponse
+
+    //lateinit var currencyResponse: GetCurrencyResponse
+
+    protected var languageCode: String? = ""
+    protected var currencyCode: String? = ""
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?)
@@ -30,7 +41,23 @@ class SettingsFragment: BaseUrlChangeFragment() {
 
         textChangeListener()
 
+        emptyInitializationSpinner()
+        //getLanguageApi()
 
+        //getCurrencyApi()
+
+    }
+
+
+    private fun emptyInitializationSpinner() = setEmptyAdapter(languageSpinner_settings, getString(R.string.language))
+
+
+    private fun setEmptyAdapter(spinner: Spinner?, hint: String)
+    {
+       /* val adapter = SpinnerAdapter(activity!!, R.layout.simple_spinner_item_black_color, ArrayList())
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        adapter.addHint(hint)
+        spinner?.adapter = adapter*/
 
     }
 }
