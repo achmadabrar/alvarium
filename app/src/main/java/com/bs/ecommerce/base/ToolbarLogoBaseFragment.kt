000f5f06e9d2cb76bfe964fc53b07467d18b0767
Ruhul Rahat <ruhul.rahat@bs-23.net>
2020-06-03 16:20:07 +0600

    toolbar logo aligned center

--- app/src/main/java/com/bs/ecommerce/base/ToolbarLogoBaseFragment.kt
+++ app/src/main/java/com/bs/ecommerce/base/ToolbarLogoBaseFragment.kt
@@ -1,7 +1,9 @@
 package com.bs.ecommerce.base
 
+import android.view.View
 import com.bs.ecommerce.R
 import com.bs.ecommerce.main.MainActivity
+import kotlinx.android.synthetic.main.toolbar.*
 
 
 abstract class ToolbarLogoBaseFragment : BaseFragment()
@@ -18,12 +20,14 @@ abstract class ToolbarLogoBaseFragment : BaseFragment()
 
 
     protected fun hideTopLogo() {
-        (activity as MainActivity).toolbarTop.logo = null
+        //(activity as MainActivity).toolbarTop.logo = null
+        (activity as MainActivity).topLogoLayout.visibility = View.GONE
     }
 
     protected fun showTopLogo() {
         activity?.title = ""
-        (activity as MainActivity).toolbarTop.setLogo(R.drawable.ic_nopstation_logo)
+        //(activity as MainActivity).toolbarTop.setLogo(R.drawable.ic_nopstation_logo)
+        (activity as MainActivity).topLogoLayout.visibility = View.VISIBLE
     }
 
 }
