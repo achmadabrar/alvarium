7cd545b1a8e64da9ed86692576aa490126d66b16
xiifwat <xiifwat@gmail.com>
2020-06-24 15:28:29 +0600

    Base url for nopCommerce 4.3 added

--- app/src/main/java/com/bs/ecommerce/networking/NetworkConstants.kt
+++ app/src/main/java/com/bs/ecommerce/networking/NetworkConstants.kt
@@ -7,7 +7,8 @@ object NetworkConstants
 {
     //@JvmStatic val SITE_URL = "http://13.72.65.213:8092"
     //@JvmStatic val SITE_URL = "https://plugin.nop-station.com"
-    @JvmStatic val SITE_URL = "https://cart.nop-station.com"
+    //@JvmStatic val SITE_URL = "https://cart.nop-station.com"
+    @JvmStatic val SITE_URL = "http://35.238.55.41:2004" // nopCommerce 4.3
 
     @JvmStatic val DEFAULT_URL = "${SITE_URL}/api/"
     @JvmStatic var BASE_URL = "${SITE_URL}/api/"
