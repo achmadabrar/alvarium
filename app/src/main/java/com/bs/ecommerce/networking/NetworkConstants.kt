2800ccec8161974978f0ecdcb3b81e6acaeffda8
xiifwat <xiifwat@gmail.com>
2020-08-13 16:32:10 +0600

    Removed unnecessary comments

--- app/src/main/java/com/bs/ecommerce/networking/NetworkConstants.kt
+++ app/src/main/java/com/bs/ecommerce/networking/NetworkConstants.kt
@@ -1,17 +1,10 @@
 package com.bs.ecommerce.networking
 
-/**
- * Created by bs-110 on 1/19/2016.
- */
 object NetworkConstants
 {
-    //@JvmStatic val SITE_URL = "http://13.72.65.213:8092"
-    //@JvmStatic val SITE_URL = "https://plugin.nop-station.com"
-    //@JvmStatic val SITE_URL = "https://cart.nop-station.com"
     @JvmStatic val SITE_URL = "http://35.238.55.41:2004" // nopCommerce 4.3
 
     @JvmStatic val DEFAULT_URL = "${SITE_URL}/api/"
     @JvmStatic var BASE_URL = "${SITE_URL}/api/"
-
 }
 
