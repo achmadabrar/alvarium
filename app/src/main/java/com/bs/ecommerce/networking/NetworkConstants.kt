5a8fa5ef5b88d196a76b802e55bdcc551fabb9f1
tawfiqur <tawfiqur.rahman@brainstation23.com>
2020-08-20 15:36:59 +0600

    code refactoring

--- app/src/main/java/com/bs/ecommerce/networking/NetworkConstants.kt
+++ app/src/main/java/com/bs/ecommerce/networking/NetworkConstants.kt
@@ -1,16 +1,10 @@
 package com.bs.ecommerce.networking
 
-/**
- * Created by bs-110 on 1/19/2016.
- */
 object NetworkConstants
 {
-    //@JvmStatic val SITE_URL = "http://13.72.65.213:8092"
-    @JvmStatic val SITE_URL = "https://plugin.nop-station.com"
-//    @JvmStatic val SITE_URL = "https://cart.nop-station.com"
+    @JvmStatic val SITE_URL = "https://plugin.nop-station.com" // nopCommerce 4.2
 
     @JvmStatic val DEFAULT_URL = "${SITE_URL}/api/"
     @JvmStatic var BASE_URL = "${SITE_URL}/api/"
-
 }
 
